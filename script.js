797ef11a6f70a06a52f8da2751c0de9162ec5b1e
Kunal Jadhav <155985737+712Kunal@users.noreply.github.com>
2026-09-03 13:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let cnt=0;
-let inc=document.getElementById("inc");
-let btn=document.getElementById("btn");
+let inc=document.getElementById("counter");
+let btn=document.getElementById("incrementBtn");
 
 btn.addEventListener("click",()=>{
 	cnt++;

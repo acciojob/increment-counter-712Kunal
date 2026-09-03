0e3e8556fb50616f0fde50ff76a57eca88cfd8f2
Kunal Jadhav <155985737+712Kunal@users.noreply.github.com>
2026-09-03 13:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let cnt=0;
+let inc=document.getElementById("inc");
+let btn=document.getElementById("btn");
+
+btn.addEventListener("click",()=>{
+	cnt++;
+	inc.innerText=cnt;
+});
